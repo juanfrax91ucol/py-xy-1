e1a2caa1eb941133a5320cbc4d99da31c919b6ef
Juan Arreola <arreola_juan@ucol.mx>
2022-05-02 17:58:48 -0500

    radious of circle inscribed in a tringle

--- 1intro/3ops/2do/inscribed.py
+++ 1intro/3ops/2do/inscribed.py
@@ -2,5 +2,20 @@
 # radious of circle inscribed in a tringle
 # place here your solution code
 #
-# zeeAlso
-# https://keisan.casio.com/exec/system/1223428152
+# Juan Francisco Arreola Hernández, arreola_juan@ucol.mx
+import math
+
+valA = input ("Ingresa A: ")
+a = float(valA)
+
+valB = input ("Ingresa B: ")
+b = float(valB)
+
+valC = input ("Ingresa C: ")
+c = float(valC)
+
+s = (1/2)*(a+b+c)
+
+r = (math.sqrt(s*(s-a)*(s-b)*(s-c)))/s
+print("Valor de s es: ", s)
+print("Valor de r es: ", r)
