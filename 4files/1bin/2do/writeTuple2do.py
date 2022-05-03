4669269042c853dce9317f60522e15f118959ca1
Juan Arreola <arreola_juan@ucol.mx>
2022-05-02 21:38:06 -0500

    writeTuple

--- 4files/1bin/2do/writeTuple2do.py
+++ 4files/1bin/2do/writeTuple2do.py
@@ -1,5 +1,14 @@
 #
 # 2do
-#
+## Juan Francisco Arreola Hernández, arreola_juan@ucol.mx
+import pickle
 
 t=12,True,3.1,'aCat'
+
+with open('tuple.bin','wb') as fh:
+        pickle.dump(t,fh)
+
+print('done...')
+
+
+
