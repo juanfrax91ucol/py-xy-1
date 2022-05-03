5924c34572c297f6b98a21c176e57e266a4b8b76
Juan Arreola <arreola_juan@ucol.mx>
2022-05-02 21:45:34 -0500

    readTuple

--- 4files/1bin/2do/readTuple2do.py
+++ 4files/1bin/2do/readTuple2do.py
@@ -1,4 +1,13 @@
 #
 # read binary file (tuple)
 # and print it
-#
+#Juan Francisco Arreola Hernández, arreola_juan@ucol.mx
+import pickle
+
+with open('tuple.bin','rb') as fh:
+        t = pickle.load(fh) 
+
+print(type(t))
+print(t)
+
+print('done...')
