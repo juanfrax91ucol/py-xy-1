8b8577b3edacbece7692b9e7ba2111e89a5d1164
Juan Arreola <arreola_juan@ucol.mx>
2022-05-05 22:07:26 -0500

    read binary file (dictionary

--- 4files/1bin/2do/readDict2do.py
+++ 4files/1bin/2do/readDict2do.py
@@ -1,4 +1,13 @@
 #
 # read binary file (dictionary)
 # and print it
-#
+#Juan Francisco Arreola Hernández, arreola_juan@ucol.mx
+import pickle
+
+with open('dict.bin','rb') as fh:
+        d = pickle.load(fh) 
+
+print(type(d))
+print(d)
+
+print('done...')
--- 4files/1bin/2do/writeDict2do.py
+++ 4files/1bin/2do/writeDict2do.py
@@ -1,6 +1,13 @@
 #
 # 2do
-#
+# Juan Francisco Arreola Hernández, arreola_juan@ucol.mx
+import pickle
 
 d={'one':'aaa aaa', 'two':'bbb bbb', 'three':'ccc ccc'}
+
+with open('dict.bin','wb') as fh:
+        pickle.dump(d,fh)
+
+print('done...')
+
 
